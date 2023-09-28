ea7176b49b2de01492c17cd98c06050c4020d066
Alexandr <alexandrgurov1@gmail.com>
2023-09-28 19:22:34 +0200

    created 3d, 4th sections. Added 5th section. Created carousel and new styles.

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+// Tiny slider
+const slider = tns({
+  container: ".carousel__inner",
+  items: 1,
+  slideBy: "page",
+  autoplay: false,
+  controls: false,
+  nav: false,
+  responsive: {
+    640: {
+      edgePadding: 20,
+      gutter: 20,
+      items: 1,
+    },
+    700: {
+      gutter: 30,
+    },
+    900: {
+      items: 1,
+    },
+  },
+});
+
+document.querySelector(".prev").addEventListener("click", function () {
+  slider.goTo("prev");
+});
+
+document.querySelector(".next").addEventListener("click", function () {
+  slider.goTo("next");
+});
+//------------------
