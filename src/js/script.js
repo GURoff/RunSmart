a7cb4127226e6abf903a40e2194e37b3e2717e95
Alexandr <alexandrgurov1@gmail.com>
2023-10-01 19:27:11 +0200

    all sections are created. adaptive. connect validateForms and mascked input.

--- src/js/script.js
+++ src/js/script.js
@@ -63,31 +63,60 @@ $(document).ready(function () {
 
   toggleSlide(".catalog-item__link");
   toggleSlide(".catalog-item__back");
+
+  //Modal windows ------
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("fast");
+  });
+
+  $(".modals__close").on("click", function () {
+    $(".overlay, #consultation, #thanks, #order").fadeOut("fast");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modals__descr").text(
+        $(".catalog-item__subtitle").eq(i).text()
+      );
+      $(".overlay, #order").fadeIn("fast");
+    });
+  });
+
+  //validate forms --------------------
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: jQuery.validator.format(
+            "At least {0} characters required!"
+          ),
+        },
+        phone: "Please specify your phone number",
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+      },
+    });
+  }
+
+  validateForms("#consultation-form");
+  validateForms("#consultation form");
+  validateForms("#order form");
+  //------------
+
+  //masked input
+  $("input[name=phone]").mask("+420 999-999-999");
 });
-
-//------------
-// $(document).ready(function ($) {
-//   $("ul.catalog__tabs").on(
-//     "click",
-//     "li:not(.catalog__tab_active)",
-//     function () {
-//       $(this)
-//         .addClass("catalog__tab_active")
-//         .siblings()
-//         .removeClass("catalog__tab_active")
-//         .closest("div.container")
-//         .find("div.catalog__content")
-//         .removeClass("catalog__content_active")
-//         .eq($(this).index())
-//         .addClass("catalog__content_active");
-//     }
-//   );
-
-//   $(".catalog-item__link").each(function (i) {
-//     $(this).on("click", function (e) {
-//       e.preventDefault();
-//       $(".catalog-item__content").toggleClass(".catalog-item__content_active");
-//       $(".catalog-item__list").toggleClass(".catalog-item__list_active");
-//     });
-//   });
-// });
